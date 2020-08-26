5e7cae8dd3d9081272d2c9d90b5143c80b6ec1c8
Alexey Klochkov <alexeywyd@gmail.com>
2020-08-26 13:06:09 +0300

    NKR-623

--- dsmart-ui-main/src/app/app.module.ts
+++ dsmart-ui-main/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {WebsocketModule} from "./websocket";
 import {environment} from '../environments/environment';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { TemperatureViewModule} from "temperature-view";
+import { SensorDataComponent } from './sensor-data/sensor-data.component';
+
+const routes: Routes =[
+  { path: '', component: HomeComponent },
+  { path: 'sensor-data', component: SensorDataComponent }
+];
 
 @NgModule({
     declarations: [
@@ -15,12 +26,23 @@ import { TemperatureViewModule} from "temperature-view";
         BrowserModule,
         WebsocketModule.config({
             url: environment.ws
-        }),
-        TemperatureViewModule,
-        FormsModule
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SensorDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    TemperatureViewModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
