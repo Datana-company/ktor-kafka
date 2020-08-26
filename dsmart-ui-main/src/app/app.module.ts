45fd820a4ce13ddf9cd03399f204b988fee08e7a
Alexey Klochkov <alexeywyd@gmail.com>
2020-08-26 15:52:20 +0300

    NKR-623 merge from dev

--- dsmart-ui-main/src/app/app.module.ts
+++ dsmart-ui-main/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {WebsocketModule} from "./websocket";
 import {environment} from '../environments/environment';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TemperatureViewModule} from "temperature-view";
 import { SensorDataComponent } from './sensor-data/sensor-data.component';
@@ -20,29 +17,21 @@ const routes: Routes =[
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        SensorDataComponent
     ],
     imports: [
         BrowserModule,
         WebsocketModule.config({
             url: environment.ws
-        })
+        }),
+        RouterModule.forRoot(routes),
+        TemperatureViewModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    SensorDataComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    TemperatureViewModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
