9618cdad90166f317de411de5d631ed1d2da3c45
Alexey Klochkov <alexeywyd@gmail.com>
2020-08-27 14:05:37 +0300

    NKR-623 смержил из дева, добавил компонент с библиотекой в проект

---
+++ dsmart-module-temerature/dsmart-module-temperature-widget/projects/temperature-view/src/lib/temperature-view.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TemperatureViewComponent } from './temperature-view.component';
+
+@NgModule({
+  declarations: [TemperatureViewComponent],
+  imports: [
+  ],
+  exports: [TemperatureViewComponent]
+})
+export class TemperatureViewModule { }
