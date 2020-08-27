65c5dbbf29bfd56c291005d69d28ef701dd3fa99
Alexey Klochkov <alexeywyd@gmail.com>
2020-08-27 14:14:37 +0300

    NKR-623 убрал ненужные компоненты

--- dsmart-ui-main/src/app/app.module.ts
+++ dsmart-ui-main/src/app/app.module.ts
@@ -1,24 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
 import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {WebsocketModule} from "./websocket";
 import {environment} from '../environments/environment';
-import { HomeComponent } from './home/home.component';
 import { TemperatureViewModule} from "temperature-view";
 import { SensorDataComponent } from './sensor-data/sensor-data.component';
 
-const routes: Routes =[
-  { path: '', component: HomeComponent },
-  { path: 'sensor-data', component: SensorDataComponent }
-];
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
         SensorDataComponent
     ],
     imports: [
@@ -26,7 +19,6 @@ const routes: Routes =[
         WebsocketModule.config({
             url: environment.ws
         }),
-        RouterModule.forRoot(routes),
         TemperatureViewModule,
         FormsModule
     ],
