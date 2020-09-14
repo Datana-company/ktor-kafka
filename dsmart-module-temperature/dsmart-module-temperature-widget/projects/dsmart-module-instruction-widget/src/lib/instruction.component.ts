077389d0435cd31a907fcd1c17ff4fa17f858333
Alexey Klochkov <alexeywyd@gmail.com>
2020-09-15 00:21:02 +0300

    NKR-686

---
+++ dsmart-module-temperature/dsmart-module-temperature-widget/projects/dsmart-module-instruction-widget/src/lib/instruction.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-dsmart-module-instruction-widget',
+  template: `
+    <p>
+      dsmart-module-instruction-widget works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class InstructionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ dsmart-module-temperature/dsmart-module-temperature-widget/projects/dsmart-module-instruction-widget/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of dsmart-module-instruction-widget
+ */
+
+export * from './lib/dsmart-module-instruction-widget.service';
+export * from './lib/instruction.component';
+export * from './lib/instruction.module';
