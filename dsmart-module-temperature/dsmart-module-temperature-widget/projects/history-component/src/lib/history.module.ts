557b75b4747e036361dd61e948fc7e3bcd49c8b7
Alexey Klochkov <alexeywyd@gmail.com>
2020-09-17 00:38:49 +0300

    NKR-703 Компонент истории сообщений

--- dsmart-module-temperature/dsmart-module-temperature-widget/projects/history-component/src/lib/history.module.ts
+++ dsmart-module-temperature/dsmart-module-temperature-widget/projects/history-component/src/lib/history.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HistoryComponent } from './history.component';
+import {RecommendationModule} from "../../../recommendation-component/src/lib/recommendation.module";
 
 
 
 @NgModule({
   declarations: [HistoryComponent],
   imports: [
+    RecommendationModule
   ],
   exports: [HistoryComponent]
 })
