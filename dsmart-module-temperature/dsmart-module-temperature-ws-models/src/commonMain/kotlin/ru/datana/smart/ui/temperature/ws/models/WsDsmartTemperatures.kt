aa69ee7eebfa1d174fb9525763a13f502bad3508
Sergey Okatov <sokatov@gmail.com>
2020-09-25 20:51:18 +0500

    NKR-744, NKR-748 - расширен формат принимаемых данных от ML

--- dsmart-module-temperature/dsmart-module-temperature-ws-models/src/commonMain/kotlin/ru/datana/smart/ui/temperature/ws/models/WsDsmartTemperatures.kt
+++ dsmart-module-temperature/dsmart-module-temperature-ws-models/src/commonMain/kotlin/ru/datana/smart/ui/temperature/ws/models/WsDsmartTemperatures.kt
@@ -5,7 +5,8 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class WsDsmartTemperatures(
     val temperature: Double? = null,
-    val timeMillis: Long? = null,
+    val timeBackend: Long? = null,
+    val timeStart: Long? = null,
     val durationMillis: Long? = null,
     val deviationPositive: Double? = null,
     val deviationNegative: Double? = null,
