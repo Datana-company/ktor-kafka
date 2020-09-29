42aa4687da5878610b1ea2e9dfd22a9e86386750
Sergey Okatov <sokatov@gmail.com>
2020-09-29 20:02:02 +0500

    NKR-744 Добавлен фильтр на sensorId, добавлена фильтрация просроченных данных: Обновление версии

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.0.25"
+version = "0.0.26"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
