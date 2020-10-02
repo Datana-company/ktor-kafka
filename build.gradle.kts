b66dcf906d3840ed96f7cf0f2bea4a164e03add7
seshlol <seshlol@icloud.com>
2020-10-02 18:18:25 +0500

    feature/NKR-695 0.0.30 версия

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.0.29"
+version = "0.0.30"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
