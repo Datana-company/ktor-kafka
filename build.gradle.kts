b9687ef95f400a8b50819e10cb55ed414f741af2
seshlol <seshlol@icloud.com>
2020-10-05 17:24:39 +0500

    feature/NKR-695 ТЕСТ

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.0.30"
+version = "0.0.31"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
