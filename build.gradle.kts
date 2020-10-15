b90067370336b19d8db82c8582c56ec27130a5b9
Sergey Okatov <sokatov@gmail.com>
2020-10-15 18:22:06 +0500

    NKR-830 Сгенерированы модели для Котлина

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.1.2"
+version = "0.1.3"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
