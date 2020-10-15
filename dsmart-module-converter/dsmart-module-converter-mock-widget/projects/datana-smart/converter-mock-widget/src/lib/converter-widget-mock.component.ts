9032aa99b1934df32d8a12f87d2a6aa66325696c
DUstinov <dustinov@lanit.ru>
2020-10-15 04:01:04 +0300

    NKR-817 - базовая структура Angular-модуля имитатора

---
+++ dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/converter-widget-mock.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'datana-converter-mock-widget',
+  template: `
+    <p>
+      datana-converter-mock-widget works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ConverterWidgetMockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/converter-widget-mock.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ConverterWidgetMockComponent } from './converter-widget-mock.component';
+
+
+
+@NgModule({
+  declarations: [ConverterWidgetMockComponent],
+  imports: [
+  ],
+  exports: [ConverterWidgetMockComponent]
+})
+export class ConverterWidgetMockModule { }
