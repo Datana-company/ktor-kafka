458941c1fb62ad70e88b24538f67eeb7d0346cca
vladimir <VYakovlev@lanit.ru>
2020-10-16 17:52:32 +0500

    NKR-807 Добавил в пути hash (для исправление бага с редиректом) В main-ui добавил навигационную панель

---
+++ dsmart-ui-main/dsmart-ui-main-front/src/app/navigate-panel/navigate-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigate-panel',
+  templateUrl: './navigate-panel.component.html',
+  styleUrls: ['./navigate-panel.component.css']
+})
+export class NavigatePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
