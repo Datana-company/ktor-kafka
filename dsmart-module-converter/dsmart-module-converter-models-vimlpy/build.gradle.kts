6c1d5a29db7dc56d990bf797a17afa6fc5896a15
Sergey Okatov <sokatov@gmail.com>
2020-10-18 16:03:50 +0500

    NKR-830 Добавлены транспортные модели для Котлина и Питона: Clean up

--- dsmart-module-converter/dsmart-module-converter-models-vimlpy/build.gradle.kts
+++ dsmart-module-converter/dsmart-module-converter-models-vimlpy/build.gradle.kts
@@ -8,26 +8,13 @@ version = rootProject.version
 
 repositories {
     mavenCentral()
-//    pyGradlePyPi()
 }
 
 python {
     alwaysInstallModules = true
 }
 
-dependencies {
-//    val jacksonVersion: String by project
-//    implementation(kotlin("stdlib"))
-//    api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-
-//    testImplementation(kotlin("test"))
-//    testImplementation(kotlin("test-junit"))
-//    implementation("pypi:requests:2.9.1")
-//    implementation("pypi:mock:2.0.0")
-}
-
 val generatedSourcesDir = "$buildDir/generated"
-//kotlin.sourceSets["main"].kotlin.srcDirs("$generatedSourcesDir/src/main/kotlin")
 
 openApiGenerate {
     generatorName.set("python-flask")
@@ -40,19 +27,12 @@ openApiGenerate {
     modelPackage.set("models")
     ignoreFileOverride.set("$projectDir/.openapi-generator-ignore")
     systemProperties.apply {
-//        put("models", "")
-//        put("modelDocs", "false")
-//        put("invoker", "")
-////        put("invoker", "false")
-//        put("apis", "false")
     }
 
     configOptions.set(mapOf(
         "dateLibrary" to "string",
         "enumPropertyNaming" to "UPPERCASE",
         "packageVersion" to project.version.toString()
-//        "library" to "multiplatform",
-//        "serializationLibrary" to "jackson"
     ))
 }
 
@@ -78,17 +58,9 @@ tasks {
         }
     }
 
-    val registerToPiPy by creating(ru.vyarus.gradle.plugin.python.task.PythonTask::class.java) {
-        dependsOn(openApiGenerate)
-        dependsOn(createPypirc)
-        workDir = "$buildDir/generated"
-        command = "setup.py register"
-    }
-
     val publishToPiPy by creating(ru.vyarus.gradle.plugin.python.task.PythonTask::class.java) {
         dependsOn(openApiGenerate)
         dependsOn(createPypirc)
-//        dependsOn(registerToPiPy)
         workDir = "$buildDir/generated"
         command = "setup.py sdist upload --repository=datana-nexus --show-response"
     }
