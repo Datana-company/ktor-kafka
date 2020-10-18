46dd66cf332fa32955062aa56ad09af47074a269
Sergey Okatov <sokatov@gmail.com>
2020-10-18 22:07:03 +0500

    NKR-816 Исправил урл запроса

--- dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/converter-widget-mock.service.ts
+++ dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/converter-widget-mock.service.ts
@@ -2,22 +2,23 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from 'rxjs';
 import {MockCasesModel} from "./models/mock-cases-model";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConverterWidgetMockService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   getList(): Observable<MockCasesModel> {
-    return this.http.get<MockCasesModel>("http://localhost:8080/list")
+    return this.http.get<MockCasesModel>(`${this.router.url}/list`)
   }
 
   startCase(dir: string): Observable<any> {
     console.log(`send dir = ${dir}`)
-    return this.http.get<any>("http://localhost:8080/send", {
+    return this.http.get<any>(`${this.router.url}/send`, {
       params: {
         case: dir
       }
