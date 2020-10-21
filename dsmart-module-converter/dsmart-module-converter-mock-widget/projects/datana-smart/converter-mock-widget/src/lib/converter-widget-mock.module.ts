0fb08c2e852d8398e7114fa37e1f9c2a859a3edc
DUstinov <dustinov@lanit.ru>
2020-10-21 14:54:33 +0300

    NKR-852 - структура

--- dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/converter-widget-mock.module.ts
+++ dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/converter-widget-mock.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { ConverterWidgetMockComponent } from './converter-widget-mock.component';
+import {NgModule} from '@angular/core';
+import {ConverterWidgetMockComponent} from './converter-widget-mock.component';
 import {MockListComponent} from "./mock-list/mock-list.component";
 import {MockListItemComponent} from "./mock-list-item/mock-list-item.component";
 import {CommonModule} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
+import {FileUploadComponent} from "./file-upload/file-upload.component";
+import {CaseEditorComponent} from "./case-editor/case-editor.component";
 
 @NgModule({
-  declarations: [
-      MockListComponent,
-      MockListItemComponent,
-      ConverterWidgetMockComponent
-  ],
-  imports: [
-      CommonModule,
-      HttpClientModule
-  ],
-  exports: [ConverterWidgetMockComponent]
+    declarations: [
+        MockListComponent,
+        MockListItemComponent,
+        ConverterWidgetMockComponent,
+        FileUploadComponent,
+        CaseEditorComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule
+    ],
+    exports: [ConverterWidgetMockComponent]
 })
-export class ConverterWidgetMockModule { }
+export class ConverterWidgetMockModule {
+}
---
+++ dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/file-upload/file-upload.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpEventType, HttpResponse} from "@angular/common/http";
+import {FileUploadService} from "../case-editor/file-upload.service";
+
+@Component({
+    selector: 'file-upload-component',
+    templateUrl: './file-upload.component.html',
+    styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent implements OnInit {
+
+    percentCompleted: number = 0;
+    urlAfterUpload = '';
+    isUploaded = false;
+
+    constructor(private fuService: FileUploadService) {
+    }
+
+    upload(files: File[]) {
+        console.log('---Uploading single file---');
+        const file = files[0];
+        console.log(file.name);
+        this.isUploaded = false;
+        this.urlAfterUpload = '';
+
+        const formData = new FormData();
+        formData.append("file", file);
+        this.fuService.uploadWithProgress(formData)
+            .subscribe(event => {
+                if (event.type === HttpEventType.UploadProgress) {
+                    this.percentCompleted = Math.round(100 * event.loaded / event.total);
+                } else if (event instanceof HttpResponse) {
+                    this.isUploaded = true;
+                    this.urlAfterUpload = event.body.link;
+                }
+            });
+    }
+
+    ngOnInit(): void {
+    }
+}
