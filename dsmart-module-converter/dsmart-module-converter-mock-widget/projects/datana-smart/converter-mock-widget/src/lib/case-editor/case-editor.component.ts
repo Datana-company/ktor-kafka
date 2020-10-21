1d6c5f44f3183c8918e8dae239ac593d08170426
DUstinov <dustinov@lanit.ru>
2020-10-21 14:54:33 +0300

    NKR-852 - структура

---
+++ dsmart-module-converter/dsmart-module-converter-mock-widget/projects/datana-smart/converter-mock-widget/src/lib/case-editor/case-editor.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'case-editor-component',
+    templateUrl: './case-editor.component.html',
+    styleUrls: ['./case-editor.component.css']
+})
+export class CaseEditorComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+}
