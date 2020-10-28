d2e65eb2cfe5dd5d5cc9de70ea4bacd34d152cb0
Sergey Okatov <sokatov@gmail.com>
2020-10-28 16:54:20 +0500

    NKR-873 разработка новой версии транспортных моделей

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.2.1"
+version = "0.2.2"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
