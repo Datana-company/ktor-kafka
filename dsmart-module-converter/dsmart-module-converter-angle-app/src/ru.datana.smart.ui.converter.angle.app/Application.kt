cdd6bf172c8770157c1f4a483bb20fa1867a4bef
VKochubey <vkochubey@yandex.ru>
2020-10-29 13:03:06 +0500

    feature/NKR-876 init

---
+++ dsmart-module-converter/dsmart-module-converter-angle-app/src/ru.datana.smart.ui.converter.angle.app/Application.kt
@@ -0,0 +1,15 @@
+package ru.datana.smart.ui.converter.angle.app
+
+import io.ktor.application.*
+import io.ktor.util.*
+import ru.datana.smart.logger.datanaLogger
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+@KtorExperimentalAPI
+fun Application.module(testing: Boolean = false) {
+
+    val logger = datanaLogger(::main::class.java)
+}
