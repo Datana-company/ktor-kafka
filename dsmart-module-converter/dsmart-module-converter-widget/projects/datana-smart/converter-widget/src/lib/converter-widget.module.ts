bf1dfe409ac911bde238aa3e9a1cbe8e38575f8a
Dmitrij <belinson@lanit.ru>
2020-10-29 13:27:53 +0300

    NKR-872_версткa

--- dsmart-module-converter/dsmart-module-converter-widget/projects/datana-smart/converter-widget/src/lib/converter-widget.module.ts
+++ dsmart-module-converter/dsmart-module-converter-widget/projects/datana-smart/converter-widget/src/lib/converter-widget.module.ts
@@ -1,27 +1,23 @@
 import {NgModule} from '@angular/core';
 import {ConverterWidgetComponent} from './converter-widget.component';
 import {VideoPlayerComponent} from './video-player-component/video-player.component';
-import {BarChartComponent} from './bar-chart/bar-chart.component';
 import {ConverterWidgetRoutingModule} from "./converter-widget-routing.module";
 import {CommonModule} from "@angular/common";
 import {WebsocketModule} from "@datana-smart/websocket";
-import {TiltAngleComponentModule} from "./tilt-angle-component/tilt-angle-component.module";
-import {EventRecommendationModule} from "./event-recommendation-component/event-recommendation.module";
-import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {TiltAngleComponentComponent} from "./tilt-angle-component/tilt-angle-component.component";
+import {EventRecommendationComponent} from "./event-recommendation-component/event-recommendation.component";
 
 @NgModule({
   declarations: [
     ConverterWidgetComponent,
     VideoPlayerComponent,
-    BarChartComponent
+    TiltAngleComponentComponent,
+    EventRecommendationComponent
   ],
   imports: [
     WebsocketModule,
     CommonModule,
     ConverterWidgetRoutingModule,
-    TiltAngleComponentModule,
-    EventRecommendationModule,
-    NgxChartsModule
   ],
   exports: [ConverterWidgetComponent]
 })
