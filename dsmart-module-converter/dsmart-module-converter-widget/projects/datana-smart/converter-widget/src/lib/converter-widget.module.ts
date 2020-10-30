229c91c91a3d5650d5d4c25fc3648f3fc4da13da
Dmitrij <belinson@lanit.ru>
2020-10-30 11:24:02 +0300

    NKR-872_версткa_UI

--- dsmart-module-converter/dsmart-module-converter-widget/projects/datana-smart/converter-widget/src/lib/converter-widget.module.ts
+++ dsmart-module-converter/dsmart-module-converter-widget/projects/datana-smart/converter-widget/src/lib/converter-widget.module.ts
@@ -7,18 +7,21 @@ import {CommonModule} from '@angular/common';
 import {WebsocketModule} from '@datana-smart/websocket';
 import {TiltAngleComponent} from "./tilt-angle-component/tilt-angle.component";
 import {EventRecommendationComponent} from "./event-recommendation-component/event-recommendation.component";
+import {NgxChartsModule} from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
     ConverterWidgetComponent,
     VideoPlayerComponent,
     TiltAngleComponent,
-    EventRecommendationComponent
+    EventRecommendationComponent,
+    BarChartComponent
   ],
   imports: [
     WebsocketModule,
     CommonModule,
     ConverterWidgetRoutingModule,
+    NgxChartsModule
   ],
   exports: [ConverterWidgetComponent]
 })
