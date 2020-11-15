f401d5035f5720fad9f9b64dd03722e3a4282d51
Sergey Okatov <sokatov@gmail.com>
2020-11-15 11:39:24 +0500

    NKR-995 NKR-996 подготовка к демо, удален выбор видео-режимов

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.4.26"
+version = "0.4.27"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
