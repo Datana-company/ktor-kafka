007b6b53ea193b02d15dec91635dca04de735820
Dmitrij <belinson@lanit.ru>
2020-11-20 15:38:25 +0300

    video image NKR-979, 926.3

--- dsmart-module-converter/dsmart-module-converter-backend/src/main/kotlin/ru/datana/smart/ui/converter/backend/handlers/EncodeBase64Handler.kt
+++ dsmart-module-converter/dsmart-module-converter-backend/src/main/kotlin/ru/datana/smart/ui/converter/backend/handlers/EncodeBase64Handler.kt
@@ -21,11 +21,14 @@ object EncodeBase64Handler : IKonveyorHandler<ConverterBeContext> {
 
         try {
             val bytes = File(imagePath).readBytes()
+
+//            Для тестов
+//            val folder = if (context.frame.channel == ModelFrame.Channels.CAMERA) "camera" else "math"
+//            val bytes = File("C:\\Users\\Dmitrij\\Desktop\\Project\\datana-smart-ui\\dsmart-module-converter\\dsmart-module-converter-app/resources/images/$folder/${Random.nextInt(0, 12)}.png").readBytes()
+
             context.frame.image = Base64.getEncoder().encodeToString(bytes)
         } catch (e: Throwable) {
             println("Файл $imagePath не найден")
-//            val bytes = EncodeBase64Handler::class.java.getResource("/images/no-signal.png").readBytes()
-//            context.frame.image = Base64.getEncoder().encodeToString(bytes)
         }
     }
 
