64639517c4d5f408c2ea08ea062ba8b8f90909df
Okatov Sergey <okatov@lanit.ru>
2020-11-20 19:35:45 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ru.datana.smart.ui"
-version = "0.4.44"
+version = "0.4.45"
 
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
