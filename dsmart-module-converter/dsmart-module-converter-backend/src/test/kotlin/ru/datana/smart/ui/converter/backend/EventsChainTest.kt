13309b53184a2dc2a9f4c227ca24c053c189248e
Dmitrij <belinson@lanit.ru>
2020-12-09 17:08:28 +0300

    NKR-1112,  but without NKR-1080

--- dsmart-module-converter/dsmart-module-converter-backend/src/test/kotlin/ru/datana/smart/ui/converter/backend/EventsChainTest.kt
+++ dsmart-module-converter/dsmart-module-converter-backend/src/test/kotlin/ru/datana/smart/ui/converter/backend/EventsChainTest.kt
@@ -174,6 +174,52 @@ internal class EventsChainTest {
 
         }
     }
+    /** NKR-1080  ModelEvent.ExecutionStatus.StatusNone
+     *  последняя рекомендация не должна быть отмечена статусом "Выполнено", т. к угол наклона не изменился,
+     *  рекомендация выдалалась и плавка закончилась, последняя рекомендация должна просто уйти в историю без статуса
+     *  currentState.get().currentMeltInfo.id.isEmpty()
+     */
+//    @Test
+//    fun isExecutionStatusNoneIfMeltFinish() {
+//        runBlocking {
+//            val repository =  createRepositoryWithEventForTest(
+//                ModelEvent.EventType.STREAM_RATE_WARNING_EVENT,
+//                Instant.now().minusMillis(7000L),
+//                0.011,
+//                null,
+//                0.1,
+//                60.0,
+//                ModelEvent.Category.WARNING
+//            )
+//
+//            val converterFacade = converterFacadeTest(
+//                roundingWeight = 0.1,
+//                metalRateWarningPoint = 0.1,
+//                metalRateCriticalPoint = 0.16,
+//                reactionTime = 3000L,
+//                currentState = createCurrentStateForTest(null,60.0,null,0.011,null),
+//                converterRepository = repository
+//            )
+//
+//            val context = converterBeContextTest(
+//                reactionTime = 3000L,
+//                meltInfo = ModelMeltInfo(""),
+//                slagRate = ModelSlagRate(
+//                    steelRate = 0.011
+//                ),
+//                frame = ModelFrame(
+//                    frameTime = Instant.now()
+//                ),
+//            )
+//            converterFacade.handleMath(context)
+//
+//            assertEquals(ModelEvent.Category.WARNING, context.events.first().category)
+//            assertEquals(ModelEvent.ExecutionStatus.NONE, context.events.first().executionStatus)
+////            assertEquals(ModelEvent.ExecutionStatus.COMPLETED, context.events.first().executionStatus)
+//
+//        }
+//    }
+
     /**NKR-1041
      * ui-converter. По окончанию скачивания шлака последняя рекомендация и световой сигнал не меняют статус (остаются активными)
      */
