21673724e5cea68937a5b6f33744bdb66b74dbbb
vladimir <VYakovlev@lanit.ru>
2020-12-11 15:51:31 +0500

    NKR-1147 ui-converter. Выдача рекомендаций. Версия 0.5.5. После окончания кейса выдается еще одна рекомендация, хотя не должна
    пофиксил баг
    написал тесты

---
+++ dsmart-module-converter/dsmart-module-converter-backend/src/test/kotlin/ru/datana/smart/ui/converter/backend/EndMeltNKR1147Test.kt
@@ -0,0 +1,185 @@
+package ru.datana.smart.ui.converter.backend
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import ru.datana.smart.ui.converter.common.models.ModelEvent
+import ru.datana.smart.ui.converter.common.models.ModelEventMode
+import ru.datana.smart.ui.converter.common.models.ModelFrame
+import ru.datana.smart.ui.converter.common.models.ModelSlagRate
+import ru.datana.smart.ui.converter.repository.inmemory.EventRepositoryInMemory
+import java.time.Instant
+import kotlin.test.assertEquals
+import kotlin.test.Test
+
+/**
+ * NKR-1147
+ * Во время окончания плавки генерируется ещё одна рекомендация (исправлено)
+ */
+class EndMeltNKR1147Test {
+
+    /**
+     * NKR-1147
+     * В репозитории уже есть активная рекомендация.
+     * После окончания плавки рекомендация записывается в историю.
+     * Установлен режим рекомендаций по металлу (EVENT_MODE = STEEL).
+     */
+    @Test
+    fun endMeltTestCase1() {
+        runBlocking {
+            val repository = createRepositoryWithEventForTest(
+                eventType = ModelEvent.EventType.STREAM_RATE_CRITICAL_EVENT,
+                timeStart = Instant.now().minusMillis(1000L),
+                category = ModelEvent.Category.CRITICAL
+            )
+
+            val meltTimeout = 5000L
+            val converterFacade = converterFacadeTest(
+                roundingWeight = 0.1,
+                meltTimeout = meltTimeout,
+                currentState = createCurrentStateForTest(
+                    lastAngle = 60.0,
+                    avgSteelRate = 0.2
+                ),
+                converterRepository = repository
+            )
+
+            val context = converterBeContextTest(
+                meltInfo = defaultMeltInfoTest(),
+                slagRate = ModelSlagRate(
+                    steelRate = 0.1
+                ),
+                frame = ModelFrame(
+                    frameTime = Instant.now()
+                )
+            )
+            converterFacade.handleMath(context)
+            delay(meltTimeout + 1000L)
+
+            println(context.events)
+            assertEquals(1, context.events.size)
+        }
+    }
+
+    /**
+     * NKR-1147
+     * Репозиторий пуст.
+     * После окончания плавки создаётся рекомендация об успешной плавке.
+     * Установлен режим рекомендаций по металлу (EVENT_MODE = STEEL).
+     */
+    @Test
+    fun endMeltTestCase2() {
+        runBlocking {
+            val repository = EventRepositoryInMemory()
+
+            val meltTimeout = 5000L
+            val converterFacade = converterFacadeTest(
+                roundingWeight = 0.1,
+                meltTimeout = meltTimeout,
+                currentState = createCurrentStateForTest(
+                    lastAngle = 60.0,
+                    avgSteelRate = 0.01
+                ),
+                converterRepository = repository
+            )
+
+            val context = converterBeContextTest(
+                meltInfo = defaultMeltInfoTest(),
+                slagRate = ModelSlagRate(
+                    steelRate = 0.01
+                ),
+                frame = ModelFrame(
+                    frameTime = Instant.now()
+                )
+            )
+            converterFacade.handleMath(context)
+            delay(meltTimeout + 1000L)
+
+            println(context.events)
+            assertEquals(1, context.events.size)
+        }
+    }
+
+    /**
+     * NKR-1147
+     * В репозитории уже есть активная рекомендация.
+     * После окончания плавки рекомендация записывается в историю.
+     * Установлен режим рекомендаций по шлаку (EVENT_MODE = SLAG).
+     */
+    @Test
+    fun endMeltTestCase3() {
+        runBlocking {
+            val repository = createRepositoryWithEventForTest(
+                eventType = ModelEvent.EventType.STREAM_RATE_CRITICAL_EVENT,
+                timeStart = Instant.now().minusMillis(1000L),
+                category = ModelEvent.Category.CRITICAL
+            )
+
+            val meltTimeout = 5000L
+            val converterFacade = converterFacadeTest(
+                roundingWeight = 0.1,
+                meltTimeout = meltTimeout,
+                eventMode = ModelEventMode.SLAG,
+                currentState = createCurrentStateForTest(
+                    lastAngle = 60.0,
+                    avgSlagRate = 0.2
+                ),
+                converterRepository = repository
+            )
+
+            val context = converterBeContextTest(
+                meltInfo = defaultMeltInfoTest(),
+                slagRate = ModelSlagRate(
+                    slagRate = 0.1
+                ),
+                frame = ModelFrame(
+                    frameTime = Instant.now()
+                )
+            )
+            converterFacade.handleMath(context)
+            delay(meltTimeout + 1000L)
+
+            println(context.events)
+            assertEquals(1, context.events.size)
+        }
+    }
+
+    /**
+     * NKR-1147
+     * Репозиторий пуст.
+     * После окончания плавки создаётся рекомендация об успешной плавке.
+     * Установлен режим рекомендаций по шлаку (EVENT_MODE = SLAG).
+     */
+    @Test
+    fun endMeltTestCase4() {
+        runBlocking {
+            val repository = EventRepositoryInMemory()
+
+            val meltTimeout = 5000L
+            val converterFacade = converterFacadeTest(
+                roundingWeight = 0.1,
+                meltTimeout = meltTimeout,
+                eventMode = ModelEventMode.SLAG,
+                currentState = createCurrentStateForTest(
+                    lastAngle = 60.0,
+                    avgSlagRate = 0.01
+                ),
+                converterRepository = repository
+            )
+
+            val context = converterBeContextTest(
+                meltInfo = defaultMeltInfoTest(),
+                slagRate = ModelSlagRate(
+                    slagRate = 0.01
+                ),
+                frame = ModelFrame(
+                    frameTime = Instant.now()
+                )
+            )
+            converterFacade.handleMath(context)
+            delay(meltTimeout + 1000L)
+
+            println(context.events)
+            assertEquals(1, context.events.size)
+        }
+    }
+}
