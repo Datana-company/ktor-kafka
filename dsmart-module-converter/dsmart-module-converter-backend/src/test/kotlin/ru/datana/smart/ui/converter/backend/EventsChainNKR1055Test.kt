efe947aaff8b6fa325bd42d599371a055772d4e8
DUstinov <dustinov@lanit.ru>
2020-12-20 21:14:22 +0300

    NKR-899 - backend tests fix 2

--- dsmart-module-converter/dsmart-module-converter-backend/src/test/kotlin/ru/datana/smart/ui/converter/backend/EventsChainNKR1055Test.kt
+++ dsmart-module-converter/dsmart-module-converter-backend/src/test/kotlin/ru/datana/smart/ui/converter/backend/EventsChainNKR1055Test.kt
@@ -100,7 +100,7 @@ internal class EventsChainNKR1055Test {
             assertEquals(ModelEvent.Category.WARNING, context.events.first().category)
             assertNotEquals(ModelEvent.ExecutionStatus.FAILED, context.events.first().executionStatus)
             assertNotEquals(ModelEvent.ExecutionStatus.COMPLETED, context.events.first().executionStatus)
-            assertEquals(ModelEvent.ExecutionStatus.NONE, context.events.first().executionStatus)
+            assertEquals(ModelEvent.ExecutionStatus.STATELESS, context.events.first().executionStatus)
 
         }
     }
@@ -243,7 +243,7 @@ internal class EventsChainNKR1055Test {
 
             assertEquals(CorStatus.SUCCESS, context.status)
             assertEquals(ModelEvent.Category.WARNING, context.events.first().category)
-            assertEquals(ModelEvent.ExecutionStatus.NONE, context.events.first().executionStatus)
+            assertEquals(ModelEvent.ExecutionStatus.STATELESS, context.events.first().executionStatus)
 
         }
     }
