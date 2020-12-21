8ae777567e678e6ce212971b707bf2e3fa989a7c
DUstinov <dustinov@lanit.ru>
2020-12-21 03:54:55 +0300

    NKR-1190 - add publish config datana-smart-ktor-kafka to Nexus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import java.net.URI
+
 plugins {
+    java
+    `maven-publish`
     kotlin("jvm")
+    id("org.jetbrains.dokka") version "0.10.0"
+    maven
 }
 
 group = "ru.datana.smart.common.ktor.kafka"
@@ -19,4 +25,45 @@ dependencies {
 
     testImplementation("io.ktor:ktor-server-netty:$ktorVersion")
     testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    archiveClassifier.set("javadoc")
+    from(tasks.dokka)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+            artifact(sourcesJar)
+        }
+    }
+    repositories {
+        val nexusHost: String? = System.getenv("NEXUS_HOST")
+        val nexusUser: String? = System.getenv("NEXUS_USER")
+        val nexusPass: String? = System.getenv("NEXUS_PASS")
+        if (nexusHost != null && nexusUser != null && nexusPass != null) {
+            maven {
+                url = URI(nexusHost)
+                credentials {
+                    username = nexusUser
+                    password = nexusPass
+                }
+            }
+        }
+    }
 }
